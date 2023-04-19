7436e4f61e09819d254e770579f666e406449000
junxfhvgnju <88426856+junxfhvgnju@users.noreply.github.com>
2023-04-19 19:52:10 +0800

    4/16/18：08

--- app/src/main/java/com/xf/psychology/adapter/TestAdapter.java
+++ app/src/main/java/com/xf/psychology/adapter/TestAdapter.java
@@ -5,12 +5,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.xf.psychology.R;
 import com.xf.psychology.bean.TestBean;
+import com.xf.psychology.ui.activity.TestActivity;
 
 import java.util.List;
 
@@ -34,6 +36,11 @@ public class TestAdapter extends RecyclerView.Adapter<TestViewHolder> {
         TestBean questionBean = data.get(position);
         Log.d("TAG", "onBindViewHolder: " + questionBean + "  position->" + position);
         holder.question.setText(questionBean.question);
+        holder.btn1.setText(questionBean.A);
+        holder.btn2.setText(questionBean.B);
+        holder.btn3.setText(questionBean.C);
+        holder.btn4.setText(questionBean.D);
+        holder.btn5.setText(questionBean.E);
         holder.answerSelect.setOnCheckedChangeListener(null);
         holder.answerSelect.clearCheck();
         if (questionBean.score != -1) {
